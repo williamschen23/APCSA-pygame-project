00e100209dfa6c60128614799303c7d77d99ca65
williamschen23 <chenwill2005@gmail.com>
2022-05-19 21:41:25 -0400

    Worked on improved movement TODO Graphics

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
-
-size = WIDTH, HEIGHT = 900, 500
+from math import floor, ceil
+size = W, H = 900, 500
 WINDOW = pygame.display.set_mode(size)
 pygame.display.set_caption("Tutorial")
 FPS = 60
 pygame.init()
 clock = pygame.time.Clock()
-image = pygame.image.load("paddle.png")
+image = pygame.image.load("paddle.png").convert_alpha()
 rect = image.get_rect()
 rect2 = image.get_rect()
-rect2.x = WIDTH - 100
+rect2.x = W - 100
+rect.y = H/2 - rect.h/2
+rect2.y = H/2 - rect2.h/2
 print(rect, rect2)
+print(rect.top, rect.bottom, rect.midtop, rect.midbottom, rect.midleft, rect.midright)
+#TODO GRAPHICS
+
 
 def draw():
     WINDOW.blit(image, rect)
@@ -21,6 +26,8 @@ def draw():
 
 def main():
     run = True
+    speed = 0
+    speed2 = 0
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -33,18 +40,30 @@ def main():
                     WINDOW.fill("blue")
                 if event.key == pygame.K_r:
                     WINDOW.fill("red")
-                if event.key == pygame.K_w:
-                    rect.y -= 10
-                if event.key == pygame.K_s:
-                    rect.y += 10
-                if event.key == pygame.K_UP:
-                    rect2.y -= 10
-                if event.key == pygame.K_DOWN:
-                    rect2.y += 10
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and keys[pygame.K_s]:
+            speed = 0
+        elif keys[pygame.K_w]:
+            speed = 1
+        elif keys[pygame.K_s]:
+            speed = -1
+        else:
+            speed = 0
+        if keys[pygame.K_UP] and keys[pygame.K_DOWN]:
+            speed2 = 0
+        elif keys[pygame.K_UP]:
+            speed2 = 1
+        elif keys[pygame.K_DOWN]:
+            speed2 = -1
+        else:
+            speed2 = 0
+
+        rect.y += -speed * 5
+        rect2.y += -speed2 * 5
         draw()
     pygame.quit()
-    
+
 
 if __name__ == "__main__":
     main()
