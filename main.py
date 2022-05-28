79fa27492f276b9d9b18d9ecd2ea61c52bd7b6f0
williamschen23 <chenwill2005@gmail.com>
2022-05-28 18:11:21 -0400

    all done except organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from random import choice
+import math
 size = W, H = 900, 500
 WINDOW = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
@@ -31,7 +32,7 @@ print(player1_rect, player2_rect)
 current_time = int(pygame.time.get_ticks()/1000)
 # print(rect.top, rect.bottom, rect.midtop, rect.midbottom, rect.midleft, rect.midright)
 #TODO ORGANIZE
-MOVE = 5
+speed = 5
 
 timer = pygame.USEREVENT + 1
 pygame.time.set_timer(timer, 5000)
@@ -56,12 +57,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == timer:
+        if event.type == timer and game_active:
             if surf_height > 50:
                 surf_height -= 5
                 player_surf_scale = pygame.transform.smoothscale(player_surf, (5, surf_height))
                 player1_rect = player_surf_scale.get_rect(center=player1_rect.center)
                 player2_rect = player_surf_scale.get_rect(center=player2_rect.center)
+                speed += .2
+                speed = math.floor(speed * 10) / 10
+                print(speed)
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -77,13 +81,13 @@ while run:
     if game_active:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] and player1_rect.top > 0:
-            player1_rect.y -= MOVE
+            player1_rect.y -= speed
         if keys[pygame.K_s] and player1_rect.bottom < H:
-            player1_rect.y += MOVE
+            player1_rect.y += speed
         if keys[pygame.K_UP] and player2_rect.top > 0:
-            player2_rect.y -= MOVE
+            player2_rect.y -= speed
         if keys[pygame.K_DOWN] and player2_rect.bottom < H:
-            player2_rect.y += MOVE
+            player2_rect.y += speed
 
         ball_rect.x += ball_x_speed
         ball_rect.y += ball_y_speed
@@ -101,16 +105,16 @@ while run:
             player1_score += 1
             game_active = False
     else:
+        # reset the game while waiting for input
         game_score = font.render(f"{player1_score}   {player2_score}", False, (255, 255, 255))
-        game_score_rect = game_score.get_rect(center=(W / 2, 50))
+        game_score_rect = game_score.get_rect(center=(W/2, 50))
         WINDOW.blit(game_score, game_score_rect)
         surf_height = 100
         ball_rect.center = (W/2, H/2)
         player_surf_scale = pygame.transform.smoothscale(player_surf, (5, surf_height))
-        player1_rect = player_surf_scale.get_rect(midleft=(10, H / 2))
-        player2_rect = player_surf_scale.get_rect(midright=(W - 10, H / 2))
-
-
+        player1_rect = player_surf_scale.get_rect(midleft=(10, H/2))
+        player2_rect = player_surf_scale.get_rect(midright=(W - 10, H/2))
+        speed = 5
 
     draw()
 print(pygame.font.get_fonts())
